6a3b214401055023ca941c608e1dc425ef6673b5
DESKTOP-C1UKPBV\ville <ville.salokoski@gmail.com>
2018-01-18 22:44:26 +0200

    first push

---
+++ ArrayRamTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArrayRamTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                gagga[] array = new gagga[63000000];
+
+                Console.WriteLine("fillign array");
+                Random rnd = new Random();
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = new gagga(rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10));
+                }
+
+
+                Console.WriteLine("array filled with junk");
+                
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("perkele en toimi. Error: "+e);
+                
+            }
+            string text = Console.ReadLine();
+
+        }
+    }
+}
---
+++ ArrayRamTest/gagga.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRamTest
+{
+    class gagga
+    {
+
+        int num;
+        int num2;
+        int num3;
+
+        public gagga(int num1_, int num2_, int num3_)
+        {
+            num = num1_;
+            num2 = num2_;
+            num3 = num3_;
+        }
+    }
+}
